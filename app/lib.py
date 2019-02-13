75bb16f8cc436504f9be95cc947211a04c67bbf0
Sergey <student@mail.net>
2019-02-13 20:20:37 +0300

    Fixed #1

--- app/lib.py
+++ app/lib.py
@@ -1,11 +1,11 @@
 def calculate_bonuses(purchase_sum, purchases_total):
 
     """
-    >>> calculate_bonuses(1_000, 0)
-    0
-
     >>> calculate_bonuses(100, 1)
     0
+
+    >>> calculate_bonuses(1_000, 0)
+    50
 
     >>> calculate_bonuses(1_000, 1)
     50
@@ -15,10 +15,20 @@ def calculate_bonuses(purchase_sum, purchases_total):
 
     >>> calculate_bonuses(3_300.50, 150_000)
     300
+
+    >>> calculate_bonuses(30_000, 1_500)
+    2100
+
+    >>> calculate_bonuses(5_000, 145_000)
+    500
     """
 
-    if purchases_total == 0:
+    bonus_cost = 1_000
+
+    if purchase_sum < bonus_cost:
         return 0
+
+    purchases_total += purchase_sum
 
     if purchases_total < 15_000:
         bonus_level = 50
@@ -26,8 +36,6 @@ def calculate_bonuses(purchase_sum, purchases_total):
         bonus_level = 70
     else:
         bonus_level = 100
-
-    bonus_cost = 1_000
 
     result = int(purchase_sum // bonus_cost) * bonus_level
     return result
